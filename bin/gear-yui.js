df59e997ccaa2aa3989b42d26c8c530e1f84bd3a
Stephen Murphy <stephen@hypernaut.com>
2012-07-30 23:21:57 -0700

    updated bin script

--- bin/gear-yui.js
+++ bin/gear-yui.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
+var Queue = require('gear').Queue,
+    Registry = require('gear').Registry,
+    registry = new Registry({module: 'gear-lib'});
+
 console.log('hi');
+
+/*
+registry.load({filename: 'yui.js'});
+
+new Queue({registry: registry})
+    .read('')
+    .inspect();*/
